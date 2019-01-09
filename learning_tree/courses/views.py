2b923da5c6fe4d0feeb361db869a364399753d9f
Meshack Ogeto <sharkdevs@Meshacks-MacBook-Pro.local>
2019-01-09 08:32:01 +0300

    Test the views and models

--- learning_tree/courses/views.py
+++ learning_tree/courses/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Course
 
 # Create your views here.
+def courses(response):
+    return Course.objects.all()
